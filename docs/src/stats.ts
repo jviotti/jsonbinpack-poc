787db8dd69224a3fb274cc96505a36c3c13e41ff
Juan Cruz Viotti <jv@jviotti.com>
2021-02-23 11:59:34 -0400

    Implement basic logic
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- docs/src/stats.ts
+++ docs/src/stats.ts
@@ -16,15 +16,66 @@
 
 import * as CodeMirror from 'codemirror'
 
-const editorElement: HTMLElement | null = document.getElementById('editor')
+import {
+  JSONValue
+} from '../../lib/json'
 
+import {
+  JSONStats,
+  JSONStatsSummary,
+  summarize,
+  analyze,
+  qualify
+} from '../../utils/jsonstats'
+
+const editorElement: HTMLElement | null = document.getElementById('editor')
 if (editorElement === null) {
   throw new Error('Editor element does not exist')
 }
 
-CodeMirror(editorElement, {
+const code: CodeMirror.Editor = CodeMirror(editorElement, {
   lineNumbers: true,
   value: '{"foo":"bar"}',
   theme: 'idea',
   mode:  'json'
 });
+
+const buttonElement: HTMLElement | null = document.getElementById('analyze')
+if (buttonElement === null) {
+  throw new Error('Button element does not exist')
+}
+
+const parseJSON = (value: string): JSONValue => {
+  try {
+    return JSON.parse(value)
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      document.querySelectorAll('.error-modal').forEach((element: Element) => {
+        element.classList.remove('hidden')
+      })
+    }
+
+    throw error
+  }
+}
+
+buttonElement.addEventListener('click', () => {
+  const contents: string = code.getValue()
+  const json: JSONValue = parseJSON(contents)
+  const stats: JSONStats = analyze(json)
+  const summary: JSONStatsSummary = summarize(stats)
+  console.log(stats)
+  console.log(summary)
+  console.log(qualify(summary))
+})
+
+const modalButtonElement: HTMLElement | null = document.getElementById('close-modal')
+if (modalButtonElement === null) {
+  throw new Error('Modal button element does not exist')
+}
+
+modalButtonElement.addEventListener('click', () => {
+  document.querySelectorAll('.error-modal').forEach((element: Element) => {
+    element.classList.add('hidden')
+  })
+})
