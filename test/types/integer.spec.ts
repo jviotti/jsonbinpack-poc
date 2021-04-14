66eb5d04d700bf563fcff3d2a95dc81961d803b8
Juan Cruz Viotti <jv@jviotti.com>
2021-04-14 19:38:49 -0400

    Add basic ROOF__MIRROR_ENUM_VARINT tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/types/integer.spec.ts
+++ test/types/integer.spec.ts
@@ -142,6 +142,22 @@ tap.test('ARBITRARY_MULTIPLE__ZIGZAG_VARINT', (test) => {
   test.end()
 })
 
+tap.test('ROOF__MIRROR_ENUM_VARINT: should encode -3 (..-2) as 0x01', (test) => {
+  const buffer: Buffer = Buffer.allocUnsafe(1)
+  const bytesWritten: number = ENCODE_ROOF__MIRROR_ENUM_VARINT(buffer, 0, -3, -2)
+  test.strictSame(buffer, Buffer.from([ 0x01 ]))
+  test.is(bytesWritten, 1)
+  test.end()
+})
+
+tap.test('ROOF__MIRROR_ENUM_VARINT: should encode 8 (..10) as 0x02', (test) => {
+  const buffer: Buffer = Buffer.allocUnsafe(1)
+  const bytesWritten: number = ENCODE_ROOF__MIRROR_ENUM_VARINT(buffer, 0, 8, 10)
+  test.strictSame(buffer, Buffer.from([ 0x02 ]))
+  test.is(bytesWritten, 1)
+  test.end()
+})
+
 tap.test('ARBITRARY__ZIGZAG_VARINT: should encode -25200 as 0xdf 0x89 0x03', (test) => {
   const buffer: Buffer = Buffer.allocUnsafe(3)
   const bytesWritten: number = ENCODE_ARBITRARY__ZIGZAG_VARINT(buffer, 0, -25200)
